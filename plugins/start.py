bbe00927a0be593ca5079eb19e869db0e22c8cc2
RSR-TG <80834834+RSR-TG@users.noreply.github.com>
2021-03-21 14:39:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,8 +6,8 @@ async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
         [InlineKeyboardButton("Channel", url="https://t.me/mizolibrary")],
-        [InlineKeyboardButton("Group" , url="https://t.me/rsrtginfo
-            "Report Bugs 😊", url="https://t.me/rsrtginfo")]
+        [InlineKeyboardButton("Group" , url="https://t.me/rsrtginfo")],
+            "Report Bugs ", url="https://t.me/rsrtginfo")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b> hit\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
